0c0765a88248c28548020c4fc3a5f2b10babeefb
yuan <ayrw@163.com>
2021-09-27 22:44:28 +0800

    M

--- WaveTable.h
+++ WaveTable.h
@@ -1,11 +1,11 @@
 #ifndef __WAVETABLE__
 #define __WAVETABLE__
-// Sample name: Harp D#5
-// Sample's base frequency: 621.8548762513416 Hz
+// Sample name: Square Wave C5
+// Sample's base frequency: 524.5767916408392 Hz
 // Sample's sample rate: 32000 Hz
-#define WAVETABLE_LEN 13187
-#define WAVETABLE_ATTACK_LEN 12621
-#define WAVETABLE_LOOP_LEN 566
+#define WAVETABLE_LEN 4557
+#define WAVETABLE_ATTACK_LEN 4069
+#define WAVETABLE_LOOP_LEN 488
 
 #include <stdint.h>
 extern __code int8_t WaveTable[WAVETABLE_LEN];

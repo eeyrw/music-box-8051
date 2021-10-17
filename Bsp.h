d0151afd848feb4ec3c8bf8db8888b900e5d0f8a
yuan <ayrw@163.com>
2021-10-17 14:35:46 +0800

    M

--- Bsp.h
+++ Bsp.h
@@ -7,6 +7,7 @@ void HardwareInit(void);
 void StartAudioOutput(void);
 void StopAudioOutput(void);
 
+// These declaration must be included in main.c to generate ljmp in vector table.
 extern void timer_isr() __interrupt(TIMER0_VECTOR) __using(1);
 extern void UART1_int(void) __interrupt(UART1_VECTOR);
 

58aed26f15808fc9140a92b666f7fd6867049d6b
yuan <ayrw@163.com>
2021-10-18 22:51:01 +0800

    M

--- WavetableSynthesizer/WaveTable.h
+++ WavetableSynthesizer/WaveTable.h
@@ -1,14 +1,15 @@
 #ifndef __WAVETABLE__
 #define __WAVETABLE__
-// Sample name: CP-80 EP C4
-// Sample's base frequency: 262.2347666207412 Hz
+// Sample name: Harp D#5
+// Sample's base frequency: 619.461615976171 Hz
 // Sample's sample rate: 32000 Hz
-#define WAVETABLE_LEN 8461
-#define WAVETABLE_ATTACK_LEN 8340
-#define WAVETABLE_LOOP_LEN 121
+#define WAVETABLE_LEN 2302
+#define WAVETABLE_ATTACK_LEN 2251
+#define WAVETABLE_LOOP_LEN 51
+#define WAVETABLE_ACTUAL_LEN 2303
 
 #include <stdint.h>
-extern __code int8_t WaveTable[WAVETABLE_LEN];
+extern __code int8_t WaveTable[WAVETABLE_ACTUAL_LEN];
 extern __code uint16_t WaveTable_Increment[];
 
 #endif

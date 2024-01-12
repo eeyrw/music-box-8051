549ac2c6bdcdea52f762a48c206ae0273538531a
yuan <ayrw@163.com>
2024-01-12 13:09:02 +0800

    M

--- WavetableSynthesizer/WaveTable.h
+++ WavetableSynthesizer/WaveTable.h
@@ -1,12 +1,12 @@
 #ifndef __WAVETABLE__
 #define __WAVETABLE__
-// Sample name: CP-80 EP C4
-// Sample's base frequency: 262.24329160743173 Hz
+// Sample name: Celesta C5(L)
+// Sample's base frequency: 523.2879013149699 Hz
 // Sample's sample rate: 32000 Hz
-#define WAVETABLE_LEN 8338
-#define WAVETABLE_ATTACK_LEN 8216
-#define WAVETABLE_LOOP_LEN 122
-#define WAVETABLE_ACTUAL_LEN 8339
+#define WAVETABLE_LEN 21869
+#define WAVETABLE_ATTACK_LEN 21260
+#define WAVETABLE_LOOP_LEN 609
+#define WAVETABLE_ACTUAL_LEN 21870
 
 #include <stdint.h>
 extern __code int8_t WaveTable[WAVETABLE_ACTUAL_LEN];
